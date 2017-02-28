c89b573144f9bcec514e89121c7ea5298ddc6821
lvapeab <lvapeab@gmail.com>
2017-02-28 11:15:15 +0100

    SKIP_VECTORS_HIDDEN_SIZE = TARGET_TEXT_EMBEDDING_SIZE

--- config.py
+++ config.py
@@ -205,7 +205,7 @@ def load_parameters():
     BIDIRECTIONAL_DEEP_PREV_SENT_ENCODER = True   # Use bidirectional encoder in all encoding layers
 
     DECODER_HIDDEN_SIZE = 300                     # For models with LSTM decoder (ABiViRNet 484)
-    SKIP_VECTORS_HIDDEN_SIZE = 300
+    SKIP_VECTORS_HIDDEN_SIZE = TARGET_TEXT_EMBEDDING_SIZE
     ADDITIONAL_OUTPUT_MERGE_MODE = 'sum'          # Merge mode for the skip connections
     WEIGHTED_MERGE = False       # Wether we want to apply a conventional or a weighted merge
 
